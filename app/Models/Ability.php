763f71e3257daa64e3cb59c7ca61b286a339279b
Rico van Zelst <ricovzelst@pm.me>
2023-11-20 21:05:28 +0100

    feat: AbilityController, AbilityResource, Ability model, and abilitie...
    - Created a new file `AbilityController.php` with index, store, show, update, and destroy methods
    - Created a new file `AbilityResource.php` with toArray method
    - Removed HasFactory trait from `Ability` model
    - Added fillable fields to `Ability` model: name and effect
    - Created a new migration for the `abilities` table with id, name, effect, and timestamps columns

--- app/Models/Ability.php
+++ app/Models/Ability.php
@@ -2,20 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ability extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'name',
         'effect',
     ];
-
-    public function pokemons()
-    {
-        return $this->belongsToMany(Pokemon::class);
-    }
 }
