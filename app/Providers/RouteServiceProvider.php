2d0c1018bcb8d67da439cac411604eaab749aa57
Rico van Zelst <ricovzelst@pm.me>
2023-11-23 13:18:28 +0100

    fix: make ratelimit work

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,17 +24,18 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
-        });
-
         RateLimiter::for('pokemon', function (Request $request) {
             return Limit::perMinute(150)->by($request->ip());
         });
 
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+
+
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
+            Route::prefix('api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
