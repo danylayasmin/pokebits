62ad7bbdb37d66d85fa6042d203bef23a71a0e82
Da <166322@student.horizoncollege.nl>
2023-11-23 16:36:52 +0100

    logic for using api
    has to be tested on an online server

--- app/Http/Controllers/Frontend/PokemonController.php
+++ app/Http/Controllers/Frontend/PokemonController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class PokemonController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $client = new Client([
+            'base_uri' => 'http://127.0.0.1:8001/api/',
+            'timeout' => 2.0,
+        ]);
+
+        $response = $client->request('GET', 'pokemon');
+
+        $data = json_decode($response->getBody()->getContents());
+
+        dd($data);
     }
 }
